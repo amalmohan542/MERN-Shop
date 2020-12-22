d81a13e285e1064e74a6bf1bfaec4decc2671db7
Amal Mohan K <amalmohan542@gmail.com>
2020-12-22 14:15:51 +0530

    Basic frontend layout created using React with homepage and individual product views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to MERN Store</h1>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="py-3">
+          <Container>
+            <Route path="/" component={HomeScreen} exact />
+            <Route path="/product/:id" component={ProductScreen} exact />
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
